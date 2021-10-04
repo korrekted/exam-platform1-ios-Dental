da58099bdaf47b27013348e6231e85a0a7f3ac0b
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-10-04 16:42:47 +0300

    build 1

---
+++ Dental/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 247, green: 249, blue: 250)
+    
+    static let mainColor = UIColor(integralRed: 76, green: 180, blue: 212)
+    static let mainColorAlpha = UIColor(integralRed: 76, green: 180, blue: 212, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 177, green: 169, blue: 255)
+    
+    static let progress1Color = UIColor(integralRed: 76, green: 180, blue: 212)
+    static let progress2Color = UIColor(integralRed: 177, green: 169, blue: 255)
+    static let progress3Color = UIColor(integralRed: 97, green: 76, blue: 212)
+}
