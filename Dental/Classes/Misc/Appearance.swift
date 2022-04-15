ada25a9aedd245eaaa1c4d16f8f8dd5db2b61bfc
akonst17 <akonst17@gmail.com>
2022-04-15 13:06:11 +0300

    upgrade; build 5

--- Dental/Classes/Misc/Appearance.swift
+++ Dental/Classes/Misc/Appearance.swift
@@ -18,4 +18,8 @@ struct Appearance {
     static let progress1Color = UIColor(integralRed: 76, green: 180, blue: 212)
     static let progress2Color = UIColor(integralRed: 177, green: 169, blue: 255)
     static let progress3Color = UIColor(integralRed: 97, green: 76, blue: 212)
+    
+    static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
+    static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
+    static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
 }
--- Dental/Classes/Service/Stats/Share/StatsShareManager.swift
+++ Dental/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.dental.widgets")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.dental.widget")
     }
     
     @discardableResult
