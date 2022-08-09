5be96ffa0377e1d1087071f6fab7ad8c76074943
akonst17 <akonst17@gmail.com>
2022-08-09 11:59:58 +0300

    обновления Nursing; build 7

--- Dental/Classes/Misc/Appearance.swift
+++ Dental/Classes/Misc/Appearance.swift
@@ -8,18 +8,14 @@
 import UIKit
 
 struct Appearance {
-    static let backgroundColor = UIColor(integralRed: 247, green: 249, blue: 250)
-    
     static let mainColor = UIColor(integralRed: 76, green: 180, blue: 212)
     static let mainColorAlpha = UIColor(integralRed: 76, green: 180, blue: 212, alpha: 0.3)
-    
-    static let takeFreeTestColor = UIColor(integralRed: 177, green: 169, blue: 255)
-    
-    static let progress1Color = UIColor(integralRed: 76, green: 180, blue: 212)
-    static let progress2Color = UIColor(integralRed: 177, green: 169, blue: 255)
-    static let progress3Color = UIColor(integralRed: 97, green: 76, blue: 212)
-    
+    static let secondColor = UIColor(integralRed: 97, green: 76, blue: 212)
+    static let thirdColor = UIColor(integralRed: 177, green: 169, blue: 255)
     static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
     static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
     static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
+    static let backgroundColor = UIColor(integralRed: 247, green: 249, blue: 255)
+    static let greyColor = UIColor(integralRed: 75, green: 81, blue: 102)
+    static let blackColor = UIColor(integralRed: 19, green: 24, blue: 42)
 }
