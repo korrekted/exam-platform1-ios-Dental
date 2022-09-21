84fd5ba0284655ce7f2f5fdca6e445b4310a293d
akonst17 <akonst17@gmail.com>
2022-09-21 12:54:23 +0300

    интеграция sdk 2.0; новые урлы; build 10

--- Dental/Classes/Misc/GlobalDefinitions.swift
+++ Dental/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams.korrekted.com"
+    static let domainUrl = "https://exams3.mastrapi.com"
     static let apiKey = "M6z99HUmPaa7ZNYF"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "FL5PXcf6a0Re6Gk7"
 
     static let applicationTag = "EXAM_DENTAL"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "FL5PXcf6a0Re6Gk7"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "Bim9U6E3zDdqMFFZ"
 
     static let appStoreUrl = "https://apps.apple.com/app/danb-cda-practice-test/id1586582804"
     static let appleAppID = "1586582804"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
